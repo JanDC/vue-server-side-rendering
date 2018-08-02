d009f0e64419621e206fbcfb988daece43fc3353
Jan Decavele <jandecavele@gmail.com>
2018-08-02 14:24:09 +0200

    expanded sample with context

--- web/index.php
+++ web/index.php
@@ -1,13 +1,15 @@
 <?php
 
 
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__.'/../vendor/autoload.php';
 
-$entry = __DIR__ . '/js/index.js';
+$entry = __DIR__.'/js/index.js';
 
-$ssr = new Spatie\Ssr\Renderer(new Spatie\Ssr\Engines\Node('/usr/local/bin/node', __DIR__ . '/../cache'));
+$ssr = new Spatie\Ssr\Renderer(new Spatie\Ssr\Engines\Node(trim(`which node`), __DIR__.'/../cache'));
 $ssr->enabled();
+$ssr->context('title', 'vue test site');
 $ssr->entry($entry);
+$ssr->debug(true);
 
 
 echo $ssr->render();
