f55a7f1bf1da9b5b5bae9926a2ab7bc763823466
gijsroge <rogegijs@gmail.com>
2018-08-09 12:23:50 +0200

    added base twig

--- web/js/webpack.config.js
+++ web/js/webpack.config.js
@@ -24,3 +24,39 @@ module.exports = {
     new VueLoaderPlugin()
   ]
 }
+
+// 'use strict'
+
+// const { VueLoaderPlugin } = require('vue-loader')
+// const path = require('path');
+
+// module.exports = {
+//   mode: 'production',
+//   output: {
+//     filename: 'server.bundle.js',
+//     libraryTarget: 'commonjs2'
+//   },
+//   entry: {
+//     'entry-server': path.resolve(__dirname, './entry-client.js'),
+//   },
+//   optimization: {
+//     minimize: false,
+//   },
+//   resolve: {
+//     alias: {
+//       'vue$': 'vue/dist/vue.esm.js'
+//     },
+//     extensions: ['*', '.js', '.vue', '.json']
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test: /\.vue$/,
+//         use: 'vue-loader'
+//       }
+//     ]
+//   },
+//   plugins: [
+//     new VueLoaderPlugin()
+//   ]
+// }
