c9a6771e7b5abd952915c9ef4d2690bbbb569563
gijsroge <rogegijs@gmail.com>
2018-08-09 16:07:27 +0200

    Added css loader

--- webpack.shared.js
+++ webpack.shared.js
@@ -1,4 +1,4 @@
-const { VueLoaderPlugin } = require('vue-loader');
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
 const path = require('path');
 
 module.exports = {
@@ -8,11 +8,39 @@ module.exports = {
     },
     module: {
         rules: [
-
             {
                 test: /\.vue$/,
-                use: 'vue-loader'
-            }
+                loader: 'vue-loader'
+            },
+            /*{
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'vue-style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'vue-style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: { importLoaders: 1 }
+                    },
+                    'postcss-loader'
+                ]
+            },*/
         ]
     },
     plugins: [
