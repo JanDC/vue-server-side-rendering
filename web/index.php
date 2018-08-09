751560e421b7bcf36a8fe44d2534781b63dd5a37
Jan Decavele <jandecavele@gmail.com>
2018-08-09 11:43:22 +0200

    reference to new server entry

--- web/index.php
+++ web/index.php
@@ -3,7 +3,7 @@
 
 require_once __DIR__.'/../vendor/autoload.php';
 
-$entry = __DIR__.'/js/index.js';
+$entry = __DIR__.'/js/dist/entry-server.js';
 
 $ssr = new Spatie\Ssr\Renderer(new Spatie\Ssr\Engines\Node(trim(`which node`), __DIR__.'/../cache'));
 $ssr->enabled();
@@ -11,5 +11,6 @@ $ssr->context('title', 'vue test site');
 $ssr->entry($entry);
 $ssr->debug(true);
 
-
+echo '<pre>';
 echo $ssr->render();
+echo '</pre>';
