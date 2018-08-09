ef3ef52fe367c2c8b9813cba1a5ba1f9d36582a0
gijsroge <rogegijs@gmail.com>
2018-08-09 11:32:34 +0200

    removed packages

--- web/js/router.js
+++ web/js/router.js
@@ -1,13 +1,13 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from './components/Home.vue';
-import Packages from './components/Packages.vue';
+import Test from './components/Test.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
     { path: '/vue/', name: 'home', component: Home },
-    { path: '/vue/packages/:type', name: 'packages', component: Packages },
+    { path: '/vue/test', name: 'test', component: Test },
 ];
 
 export default new VueRouter({
