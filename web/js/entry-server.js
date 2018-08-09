6a90fa3c8f6c5036cc4236ab64370a913d3757dd
gijsroge <rogegijs@gmail.com>
2018-08-09 12:05:20 +0200

    fixed wrong paths to entry files

--- web/js/entry-server.js
+++ web/js/entry-server.js
@@ -5,11 +5,9 @@ import renderVueComponentToString from 'vue-server-renderer/basic';
 
 app.$router.push(context.url);
 
-app.$store.commit('setPackages', { packages: context.packages });
-
 renderVueComponentToString(app, (err, html) => {
     if (err) {
         throw new Error(err);
     }
-    dispatch(html);
+    print(html)
 });
--- web/js/webpack.config.js
+++ web/js/webpack.config.js
@@ -6,8 +6,11 @@ const path = require('path');
 module.exports = {
   mode: 'production',
   entry: {
-    'entry-client': path.resolve(__dirname, './entry-server.js'),
-    'entry-server': path.resolve(__dirname, './entry-client.js'),
+    'entry-client': path.resolve(__dirname, './entry-client.js'),
+    'entry-server': path.resolve(__dirname, './entry-server.js'),
+  },
+  optimization: {
+    minimize: false,
   },
   module: {
     rules: [
