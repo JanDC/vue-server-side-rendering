f85ec92307a7f108780d99241441d23a2dd93bd0
gijsroge <rogegijs@gmail.com>
2018-10-05 16:00:49 +0200

    Added bootstrap & register form

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -9,31 +9,27 @@ module.exports = {
   head: {
     title: pkg.name,
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
+      {charset: 'utf-8'},
+      {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+      {hid: 'description', name: 'description', content: pkg.description}
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'}]
   },
 
   /*
   ** Customize the progress-bar color
   */
-  loading: { color: '#fff' },
+  loading: {color: '#fff'},
 
   /*
   ** Global CSS
   */
-  css: [
-  ],
+  css: ['@/assets/scss/main.scss','@/assets/scss/bootstrap.scss'],
 
   /*
   ** Plugins to load before mounting the App
   */
-  plugins: [
-  ],
+  plugins: [],
 
   /*
   ** Nuxt.js modules
@@ -57,15 +53,6 @@ module.exports = {
     ** You can extend webpack config here
     */
     extend(config, ctx) {
-      // Run ESLint on save
-      if (ctx.isDev && ctx.isClient) {
-        config.module.rules.push({
-          enforce: 'pre',
-          test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
-          exclude: /(node_modules)/
-        })
-      }
     }
   }
-}
+};
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,66 +1,33 @@
 <template>
   <section class="container">
-    <div>
-      <logo/>
-      <h1 class="title">
-        condor-frontend
-      </h1>
-      <h2 class="subtitle">
-        My top-notch Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
+
+    <form class="d-flex vh-100 p-3 mx-auto flex-column justify-content-center">
+      <div class="form-group">
+        <label for="register-email-1">Email</label>
+        <input type="email" class="form-control" id="register-email-1">
       </div>
-    </div>
+      <div class="form-group">
+        <label for="register-password-1">Wachtwoord</label>
+        <input type="password" class="form-control" id="register-password-1">
+      </div>
+      <button class="btn btn-primary">
+        Registreer
+      </button>
+    </form>
+
   </section>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+  import Logo from '~/components/Logo.vue'
 
-export default {
-  components: {
-    Logo
+  export default {
+    components: {
+      Logo
+    }
   }
-}
 </script>
 
-<style>
+<style lang="scss">
 
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
